82f28a575dc1cf9dd5f85621d33394e2d0c09822
James Macharia <jamesmasher13@gmail.com>
2018-10-16 15:33:09 +0300

    Add permutations

---
+++ DataStructures/permutation.js
@@ -0,0 +1,20 @@
+const permutations = string => {
+  let res = [];
+
+  if (string.length === 1) {
+    res.push(string);
+    return res;
+  }
+
+  for (let i = 0; i < string.length; i++) {
+    let curLet = string[i];
+    let newStr = string.substring(0, i) + string.substring(i + 1);
+    let recursiveRes = permutations(newStr);
+
+    for (let j = 0; j < recursiveRes.length; j++) {
+      res.push(curLet + recursiveRes[j]);
+    }
+  }
+  return [...new Set(res)];
+};
+console.log(permutations("aabb"));
