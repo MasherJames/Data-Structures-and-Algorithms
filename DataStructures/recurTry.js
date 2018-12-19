04eb6dfa8f11afd9e13aa1c231f06aa6bb6328de
James Macharia <jamesmasher13@gmail.com>
2018-12-19 14:25:55 +0300

    Add partition

---
+++ DataStructures/recurTry.js
@@ -0,0 +1,12 @@
+// const arrDiff = (arr1, arr2) => arr1.filter(e => arr2.indexOf(e) === -1);
+// console.log(arrDiff([1, 2], [1]));
+const isEven = n => n % 2 === 0;
+const partitionOn = (pred, items) => {
+  const even = items.filter(e => func(e));
+  const odd = items.filter(e => even.indexOf(e) === -1);
+  const both = [...odd, ...even];
+  const indx = both.indexOf(even[0]);
+  return indx;
+};
+console.log(partitionOn(isEven, [1, 2, 3, 4, 5, 6]));
+// const isEven = n => n % 2 === 0;
--- DataStructures/unique.py
+++ DataStructures/unique.py
@@ -1,6 +1,11 @@
-def findUniq(arr):
-    for i in range(len(arr)):
-        currNum = arr[i]
-        if currNum != arr[i+1]:
-            return arr[i + 1]
-print(findUniq([1, 1, 1, 0.55,  1, 1, ]))
+# def findUniq(arr):
+#     for i in range(len(arr)):
+#         currNum = arr[i]
+#         if currNum != arr[i+1]:
+#             return arr[i + 1]
+# print(findUniq([1, 1, 1, 0.55,  1, 1, ]))
+
+
+def arr_dif(arr1, arr2):
+    return [e for e in arr1 if e not in arr2]
+print(arr_dif([1, 2, 2, 2, 3], [2]))
