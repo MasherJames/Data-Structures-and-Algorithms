77e44fb3811e5b680b2a2452b7b52a15d303ca84
James Macharia <jamesmasher13@gmail.com>
2018-12-25 09:34:47 +0300

    add convertFracs

--- DataStructures/lcm.py
+++ DataStructures/lcm.py
@@ -1,4 +1,7 @@
-def getLcm(x, y):
+from functools import reduce
+
+
+def lcm(x, y):
     if x > y:
         greater = x
     else:
@@ -9,4 +12,14 @@ def getLcm(x, y):
             break
         greater += 1
     return lcm
-print(getLcm(3, 4))
+
+
+def convertFracs(lst):
+    res = []
+    denominators = [e[1] for e in lst]
+    commonDenom = reduce(lambda a, b: lcm(a, b), denominators)
+    for elem in lst:
+        numen = commonDenom / elem[1]
+        res.append([numen, commonDenom])
+    return res
+print(convertFracs([(1, 2), (1, 3), (1, 4)]))
