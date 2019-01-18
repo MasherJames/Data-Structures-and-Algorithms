0df92081727bc91e2fa180c6e2279b5bbd207c87
James Macharia <jamesmasher13@gmail.com>
2019-01-18 10:35:31 +0300

    Add fibonacci o(n)

--- DataStructures/fibonacci.js
+++ DataStructures/fibonacci.js
@@ -9,17 +9,31 @@
 // };
 // console.log(fibonacci(0));
 
-const appendObjArr = str => {
-  const res = [];
-  const strArr = str.split("");
-  for (let i = 0; i < strArr.length; i++) {
-    let val = strArr[i];
-    res.push({ val });
-    if (res.indexOf({ val }) < 0) {
-      val = 0;
-    }
-    val++;
+// const appendObjArr = str => {
+//   const res = [];
+//   const strArr = str.split("");
+//   for (let i = 0; i < strArr.length; i++) {
+//     let val = strArr[i];
+//     res.push({ val });
+//     if (res.indexOf({ val }) < 0) {
+//       val = 0;
+//     }
+//     val++;
+//   }
+//   return res;
+// };
+// console.log(appendObjArr("stress"));
+
+//Fibonacci of time complexity o(n)
+const fibbonaci = n => {
+  let a = 0,
+    b = 1,
+    c;
+  for (let i = 2; i <= n; i++) {
+    c = a + b;
+    a = b;
+    b = c;
   }
-  return res;
+  return b;
 };
-console.log(appendObjArr("stress"));
+console.log(fibbonaci(9));
