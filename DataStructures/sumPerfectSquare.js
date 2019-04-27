367ab91d83143efc878f727d44d809f1a7c7a7a4
James Macharia <jamesmasher13@gmail.com>
2019-04-27 13:51:35 +0300

    Add all

--- DataStructures/sumPerfectSquare.js
+++ DataStructures/sumPerfectSquare.js
@@ -1,2 +1,14 @@
-const SumsPerfectSquare = num => {};
-console.log(SumsPerfectSquare());
+const SumsPerfectSquare = num => {
+  let squares = [];
+  let all = [];
+  let sum = 0;
+  for (let i = 1; i <= Math.sqrt(num); i++) {
+    squares.push(i * i);
+  }
+  for (let sq of squares) {
+    sum += sq;
+    all.push(sq);
+  }
+  return squares;
+};
+console.log(SumsPerfectSquare(15));
--- DataStructures/sumPerfectSquare.py
+++ DataStructures/sumPerfectSquare.py
@@ -1,15 +1,32 @@
-def getMinSquares(n):
+# def getMinSquares(n):
+#     if n <= 3:
+#         return n
+#     res = n
+#     for x in range(1, n+1):
+#         temp = x * x
+#         if temp > n:
+#             break
+#         else:
+#             res = min(res, 1 + getMinSquares(n - temp))
+
+#     return res
+
+
+# print(getMinSquares(15))
+import math
+
+
+def numSquares(n):
     if n <= 3:
         return n
-    res = n
-    for x in range(1, n+1):
-        temp = x * x
-        if temp > n:
-            break
-        else:
-            res = min(res, 1 + getMinSquares(n - temp))
+    squares = {i * i for i in range(1, int(math.floor(math.sqrt(n))) + 1)}
+    sums = squares
+    for i in range(1, n):
+        if n in sums:
+            return i
+        sums = {a + b for a in squares for b in sums if a + b <= n}
+        print(sums)
+    return sums
 
-    return res
 
-
-print(getMinSquares(6))
+print(numSquares(15))
