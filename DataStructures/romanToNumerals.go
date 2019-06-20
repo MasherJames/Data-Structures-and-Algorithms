fb8cdbb524228c73f77ed0d215bdf760a25b238b
MasherJames <jamesmasher13@gmail.com>
2019-06-20 09:18:16 -0700

    Update on go kata

--- DataStructures/romanToNumerals.go
+++ DataStructures/romanToNumerals.go
@@ -13,18 +13,18 @@ func Decode(roman string) int {
 	numerals := map[string]int{"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
 	romanArr := strings.Split(roman, "")
 	sum := 0
-	for i := 1; i < len(romanArr); i++ {
+	for i := 0; i < len(romanArr); i++ {
+		fmt.Println(sum)
 		if i == 0 {
 			sum += numerals[romanArr[i]]
-		} else {
+		} else if i > 0 {
 			if numerals[romanArr[i]] > numerals[romanArr[i-1]] {
 				sum += numerals[romanArr[i]]
-				sum -= numerals[romanArr[i]] * 2
+				sum -= numerals[romanArr[i-1]] * 2
 			} else {
 				sum += numerals[romanArr[i]]
 			}
 		}
-
 	}
 	return sum
 }
