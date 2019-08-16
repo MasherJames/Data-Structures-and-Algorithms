bc48026bb9cf136a953cdd40c950f8b3eaecd30a
James Macharia <jamesmasher13@gmail.com>
2019-08-17 00:30:18 +0300

    Add all

--- DataStructures/permutation.js
+++ DataStructures/permutation.js
@@ -1,4 +1,5 @@
-const permutations = string => {
+const permutations = args => {
+  const string = args.toString();
   let res = [];
 
   if (string.length === 1) {
@@ -10,11 +11,23 @@ const permutations = string => {
     let curLet = string[i];
     let newStr = string.substring(0, i) + string.substring(i + 1);
     let recursiveRes = permutations(newStr);
-    console.log(recursiveRes);
     for (let j = 0; j < recursiveRes.length; j++) {
       res.push(curLet + recursiveRes[j]);
     }
   }
   return [...new Set(res)];
 };
-console.log(permutations("aabb"));
+// console.log(permutations(1027));
+
+const nextSmaller = number => {
+  const combinations = permutations(number);
+  const descendingArr = combinations.sort((f, s) => s - f);
+  const nextSmall = descendingArr[descendingArr.indexOf(String(number)) + 1];
+  return nextSmall === undefined
+    ? -1
+    : nextSmall[0] === "0"
+    ? -1
+    : Number(nextSmall);
+};
+
+console.log(nextSmaller(1027));
