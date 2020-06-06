3bec98e7d12ff7c2f147a3fd7731211910b19f80
James Macharia <jamesmasher13@gmail.com>
2020-06-07 02:44:43 +0300

    Add black jack score

--- DataStructures/BlackJackScorer.js
+++ DataStructures/BlackJackScorer.js
@@ -1,67 +1,58 @@
-// const combination = arr => {
-//   const res = arr.reduce(
-//     (acc, cur, i) => acc.concat(arr.slice(i + 1).map(e => cur + e)),
-//     []
-//   );
-//   return [...new Set(res)];
-// };
+// const scoreHand = (cards) => {
+//   let total = 0;
+//   const jkq = ["J", "K", "Q"];
+//   let lowerAces = 0;
 
-// const possibleRes = arr => {
-//   const greaterThan21 = arr.filter(e => e > 21);
-//   const lessThan21 = arr.filter(e => e <= 21);
-
-//   return lessThan21.length
-//     ? Math.max(...lessThan21)
-//     : Math.min(...greaterThan21);
-// };
-// const scoreHand = cards => {
-//   const thereAreNumbers = cards.some(e => parseInt(e, 10));
-//   const asArr = cards.filter(e => e === "A");
-//   const asVal = [];
-//   let possibleSums = [];
-//   let singleSum = 0;
-
-//   if (cards.includes("K") || cards.includes("Q") || cards.includes("J")) {
-//     const kqjs = cards.filter(e => e === "K" || e === "Q" || e === "J");
-//     kqjs.forEach(e => {
-//       singleSum += 10;
-//     });
-//   }
-//   if (thereAreNumbers) {
-//     const nums = cards.filter(e => parseInt(e, 10));
-//     nums.forEach(e => {
-//       singleSum += parseInt(e);
-//     });
+//   for (let card of cards) {
+//     if (jkq.includes(card)) {
+//       total += 10;
+//     } else if (card === "A") {
+//       total += 11;
+//       lowerAces += 1;
+//     } else {
+//       total += Number(card);
+//     }
 //   }
 
-//   if (asArr.length) {
-//     asArr.forEach(e => {
-//       asVal.push(1);
-//       asVal.push(11);
-//     });
-
-//     const possibleAsSum = asVal.length > 2 ? combination(asVal) : asVal;
-
-//     possibleSums = possibleAsSum.map(asSum => asSum + singleSum);
+//   for (let i = 1; i <= lowerAces; i++) {
+//     if (total > 21) {
+//       total -= 10;
+//     }
 //   }
-
-//   return possibleSums.length ? possibleRes(possibleSums) : singleSum;
+//   return total;
 // };
-// console.log(scoreHand(["4", "3", "A", "9", "J", "A"]));
 
-const scoreHand = cards => {
-  let noAsSum = 0;
-  const asCards = cards.filter(element => element === "A");
+const scoreHand = (cards) => {
+  let total = 0;
+  const jkq = ["J", "K", "Q"];
+  let aces = cards.filter((e) => e === "A");
+
   for (let card of cards) {
-    const int = parseInt(card);
-    if (int) {
-      noAsSum += int;
-    }
-    if (["J", "Q", "K"].includes(card)) {
-      noAsSum += 10;
+    if (jkq.includes(card)) {
+      total += 10;
+    } else if (card === "A") {
+      total += 1;
+    } else {
+      total += Number(card);
     }
   }
-  const kk = asCards.reduce((acc, cur) => {}, []);
-  return noAsSum;
+  for (let _ in aces) {
+    if (total < 12) {
+      total += 10;
+    }
+  }
+  return total;
 };
-console.log(scoreHand(["5", "4", "3", "2", "A", "K"]));
+
+// Calculate scores with cards 2-10
+console.log(scoreHand(["2", "3"]));
+console.log(scoreHand(["7", "7", "8"]));
+console.log(scoreHand(["4", "7", "8"]));
+
+// should score J, Q and K as 10
+console.log(scoreHand(["K", "J", "Q"]));
+
+// should core hands with Aces correctly
+console.log(scoreHand(["A", "3"]));
+console.log(scoreHand(["A", "A"]));
+console.log(scoreHand(["A", "2", "A", "9", "9"]));
