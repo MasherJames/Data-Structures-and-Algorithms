658520979e1ae062c318fd60329ac6938905b6be
James Macharia <jamesmasher13@gmail.com>
2020-09-12 22:49:33 +0300

    Add stuff

--- SolutionsToProblems/Maths.py
+++ SolutionsToProblems/Maths.py
@@ -25,58 +25,56 @@ import math
 
 # print(findPrimeFactors(315))
 
-# Sieve of erathosnese
-# def sieveOfErathosnese(n):
-#     # Mark all numbers as True
-#     primes = [True for i in range(n + 1)]
-#     p = 2
+def sieveOfEratosthenese(n):
+    # Mark all the numbers as prime
+    primes = [True for val in range(n+1)]
 
-#     # loop only if value of p**2 is less or equal to the given n
-#     # this because a number less than p**2 is already cancelled be another factor
-#     while(p**2 <= n):
-#         if (primes[p]):
-#             # cancel all the numbers divisible by the current num
-#             for i in range(p*p, n+1, p):
-#                 primes[i] = False
-#         p += 1
+    # 0 and 1 are defintely not primes
+    primes[0] = False
+    primes[1] = False
 
-#     return [p for p in range(2, n+1) if primes[p]]
-
-
-# print(sieveOfErathosnese(10))
+    # Loop until sqrt of n and remove multiples of the current prime
+    # At the sqrt of n, the program already knows one of the prime factorts
+    for i in range(2, int(math.sqrt(n))):
+        # Mark all prime numbers common multiples false
+        if primes[i]:
+            for j in range(i * i, n+1, i):
+                primes[j] = False
+    return [p for p in range(0, n+1) if primes[p]]
+print(sieveOfEratosthenese(100))
 
 
 # least prime factor
-# def leastPrimeFactor(n):
-#     # An array with n number of zeros
-#     least_prime_factors = [0] * (n + 1)
-#     # Mark initial pos as 1. i.e 1 is is own l.p.f
-#     least_prime_factors[1] = 1
+def leastPrimeFactor(n):
+    # An array with n number of zeros
+    least_prime_factors = [0] * (n + 1)
+    # Mark initial pos as 1. i.e 1 is is own l.p.f
+    least_prime_factors[1] = 1
 
-#     # Loop from 2 to n
-#     for i in range(2, n+1):
-#         # Check if the current pos is already marked
-#         if (least_prime_factors[i]) == 0:
-#             # Mark the current prime as the l.p.f of itself
-#             least_prime_factors[i] = i
+    # Loop from 2 to n
+    for i in range(2, n+1):
+        # Check if the current pos is already marked
+        if (least_prime_factors[i]) == 0:
+            # Mark the current prime as the l.p.f of itself
+            least_prime_factors[i] = i
 
-#             # loop the array to cancel nos. divisible by the current prime
-#             for j in range(2*i, n+1, i):
-#                 if least_prime_factors[j] == 0:
-#                     least_prime_factors[j] = i
+            # loop the array to cancel nos. divisible by the current prime
+            for j in range(2*i, n+1, i):
+                if least_prime_factors[j] == 0:
+                    least_prime_factors[j] = i
 
-#     return least_prime_factors
+    return least_prime_factors
 
 
 # print(leastPrimeFactor(6))
 
 # GCD
-def gcd(a, b):
-    if a == 0:
-        return b
+# def gcd(a, b):
+#     if a == 0:
+#         return b
         # Divide with the smaller number until the remainder is zero
         # return the prev a
-    return gcd(b % a, a)
+    # return gcd(b % a, a)
 
 
 # print(gcd(36, 15))
@@ -85,8 +83,8 @@ def gcd(a, b):
 
 # Lcm(a, b) = (a * b) / gcd(a, b)
 
-def lcm(a, b):
-    return (a*b) / gcd(a, b)
+# def lcm(a, b):
+#     return (a*b) / gcd(a, b)
 
 
-print(lcm(10, 20))
+# print(lcm(10, 20))
